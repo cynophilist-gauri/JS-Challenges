db721d418588485413e5d8b14f05874c4e7c00eb
Gauri Agarwal <95081091+cynophilist-gauri@users.noreply.github.com>
2024-07-14 04:36:40 +0530

    Add files via upload

---
+++ day_1.js
@@ -0,0 +1,40 @@
+//Task 1:
+var numberValue = 5
+console.log(numberValue);
+
+//Task 2:
+let stringValue = "Gauri"
+console.log(stringValue)
+
+//Task 3:
+const booleanValue = true
+console.log(booleanValue);
+
+//Task 4:
+objectValue = {
+    username: "Gauri",
+    password: "123",
+    loggedIn: false
+}
+
+arrayValue = [1,2,3,4]
+
+console.log(typeof numberValue);
+console.log(typeof stringValue);
+console.log(typeof booleanValue);
+console.log(typeof objectValue);
+console.log(typeof arrayValue);
+
+//Task 5:
+let value = 10
+console.log(value)
+
+value = 20
+console.log(value);
+
+//Task 6:
+const anotherValue = 10
+console.log(anotherValue);
+
+// anotherValue = 20
+// console.log(anotherValue);
