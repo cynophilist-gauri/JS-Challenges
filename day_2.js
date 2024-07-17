f8bdfaad9e67d2ab8d20264079dab1db75e488c3
Gauri Agarwal <95081091+cynophilist-gauri@users.noreply.github.com>
2024-07-17 10:04:22 +0530

    Add files via upload

---
+++ day_2.js
@@ -0,0 +1,60 @@
+const firstNumber = 20
+const secondNumber = 10
+
+//Task 1:
+const sum = firstNumber + secondNumber
+console.log("Sum of", firstNumber, "and", secondNumber, "is", sum);
+
+//Task 2:
+const difference =  firstNumber - secondNumber
+console.log("Difference of", firstNumber, "and", secondNumber, "is", difference);
+
+//Task 3:
+const product = firstNumber * secondNumber
+console.log("Product of", firstNumber, "and", secondNumber, "is", product);
+
+//Task 4:
+const quotient = firstNumber / secondNumber
+console.log("Quotient of", firstNumber, "and", secondNumber, "is", quotient);
+
+//Task 5:
+const remainder = firstNumber % secondNumber
+console.log("Remainder of", firstNumber, "and", secondNumber, "is", remainder);
+
+//Task 6:
+let number = 10
+console.log(number);
+number += 5
+console.log(number);
+
+//Task 7:
+console.log(number);
+number -= 5
+console.log(number);
+
+//Task 8:
+if(firstNumber > secondNumber) console.log(firstNumber, ">", secondNumber);
+else if(firstNumber < secondNumber) console.log(firstNumber, "<", secondNumber);
+
+//Task 9:
+if(number >= secondNumber) console.log(firstNumber, ">=", secondNumber);
+if(firstNumber <= secondNumber) console.log(firstNumber, "<=", secondNumber);
+
+//Task 10:
+const numberValue = 20
+const stringValue = "20"
+if(numberValue == stringValue) console.log("NOT OK!");
+if(numberValue === stringValue) console.log("OK!"); 
+
+//Task 11:
+if(10>5 && 10>9) console.log("10 is greater");
+
+//Task 12:
+if(5<10 && 9<10) console.log("5 is smaller");
+
+//Task 13:
+booleanValue = false
+if(!booleanValue) console.log("OH YES!");
+
+//Task 14:
+console.log(number>0 ? "Positive" : "Negative");
