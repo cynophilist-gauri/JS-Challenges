17efb351db0c20839471a5f05effe7f58eabe33d
Gauri Agarwal <95081091+cynophilist-gauri@users.noreply.github.com>
2024-07-17 19:07:19 +0530

    Update day_2.js

--- day_2.js
+++ day_2.js
@@ -1,60 +1,60 @@
 const firstNumber = 20
 const secondNumber = 10
 
-//Task 1: Write a program to add two numbers and log the result to the console.
+//Task 1:
 const sum = firstNumber + secondNumber
-console.log("Sum of", firstNumber, "and", secondNumber, "is", sum);
+console.log(`Sum of ${firstNumber} and ${secondNumber} is ${sum}`);
 
-//Task 2: Write a program to subtract two numbers and log the result to the console.
+//Task 2:
 const difference =  firstNumber - secondNumber
-console.log("Difference of", firstNumber, "and", secondNumber, "is", difference);
+console.log(`Difference of ${firstNumber} and ${secondNumber} is ${difference}`);
 
-//Task 3: Write a program to multiply two numbers and log the result to the console.
+//Task 3:
 const product = firstNumber * secondNumber
-console.log("Product of", firstNumber, "and", secondNumber, "is", product);
+console.log(`Product of ${firstNumber} and ${secondNumber} is ${product}`);
 
-//Task 4: Write a program to divide two numbers and log the result to the console.
+//Task 4:
 const quotient = firstNumber / secondNumber
-console.log("Quotient of", firstNumber, "and", secondNumber, "is", quotient);
+console.log(`Quotient of ${firstNumber} and ${secondNumber} is ${quotient}`);
 
-//Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+//Task 5:
 const remainder = firstNumber % secondNumber
-console.log("Remainder of", firstNumber, "and", secondNumber, "is", remainder);
+console.log(`Remainder of ${firstNumber} and ${secondNumber} is ${remainder}`);
 
-//Task 6: Use the += operator to add a number to a variable and log the result to the console.
+//Task 6:
 let number = 10
 console.log(number);
 number += 5
 console.log(number);
 
-//Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+//Task 7:
 console.log(number);
 number -= 5
 console.log(number);
 
-//Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+//Task 8:
 if(firstNumber > secondNumber) console.log(firstNumber, ">", secondNumber);
 else if(firstNumber < secondNumber) console.log(firstNumber, "<", secondNumber);
 
-//Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+//Task 9:
 if(number >= secondNumber) console.log(firstNumber, ">=", secondNumber);
 if(firstNumber <= secondNumber) console.log(firstNumber, "<=", secondNumber);
 
-//Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+//Task 10:
 const numberValue = 20
 const stringValue = "20"
 if(numberValue == stringValue) console.log("NOT OK!");
 if(numberValue === stringValue) console.log("OK!"); 
 
-//Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+//Task 11:
 if(10>5 && 10>9) console.log("10 is greater");
 
-//Task 12: Write a program that uses the Il operator to combine two conditions and log the result to the console.
+//Task 12:
 if(5<10 && 9<10) console.log("5 is smaller");
 
-//Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+//Task 13:
 booleanValue = false
 if(!booleanValue) console.log("OH YES!");
 
-//Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+//Task 14:
 console.log(number>0 ? "Positive" : "Negative");
